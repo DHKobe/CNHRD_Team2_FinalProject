910a50dcaf950e93721b73cfe7738187424bdde1
DHK <kimdh3273@gmail.com>
2024-10-15 17:04:41 +0900

    Feat: mainpage에 콤보박스추가
    메뉴간 이동가능한 콤보박스

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace CNHRD_Team2_FinalProject.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap 제목_없음1 {
+            get {
+                object obj = ResourceManager.GetObject("제목 없음1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
